6febd0291e350655ce567da7183abdbe5cea2fa3
RKamey <ralonso@ucol.mx>
2023-10-22 17:34:10 -0600

    Primera parte

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button, Switch } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      switchValue : false,
+    }
+  }
+
+  onPressLearnMore() {
+    console.warn(`Presionaste el boton de "Learn More"`)
+  }
+
+  onChange = value => {
+    console.warn(`El valor del switch es: ${value}`)
+    this.setState({switchValue: value})
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button
+          onPress={this.onPressLearnMore}
+          title={"Learn More"}
+          color="#841584"
+          accesibilityLabel="Learn more about this purple button"
+        />
+        <Text 
+          style={styles.text}>
+          Esto es React Native   
+        </Text>
+        <Switch
+          onValueChange={() => this.onChange(!this.state.switchValue)}
+          value = {this.state.switchValue}
+        />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1f2026',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    color: '#fff',
+    fontSize: 30,
+  },
+  image: {
+    marginTop: 10,
+    width: 150,
+    height: 150,
+  }
 });
